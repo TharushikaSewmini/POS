6558a75cf6dde39a04c4c603078c3a7a0039283e
Tharushika <tharushikasewmini@gmail.com>
2022-05-02 19:09:42 +0530

    Refactored Item Exist Method in PlaceOrderFormController

--- Layered_Architecture/src/controller/PlaceOrderFormController.java
+++ Layered_Architecture/src/controller/PlaceOrderFormController.java
@@ -192,10 +192,8 @@ public class PlaceOrderFormController {
     }
 
     private boolean existItem(String code) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("SELECT code FROM Item WHERE code=?");
-        pstm.setString(1, code);
-        return pstm.executeQuery().next();
+        ItemDAOImpl itemDAO = new ItemDAOImpl();
+        return itemDAO.existItem(code);
     }
 
     boolean existCustomer(String id) throws SQLException, ClassNotFoundException {
