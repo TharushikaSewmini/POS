5c12fa6081bae03eebf4dc911eede059a5cf90c3
Tharushika <tharushikasewmini@gmail.com>
2022-05-12 16:46:49 +0530

    Singleton Design Pattern

---
+++ Layered_Architecture/src/dao/DAOFactory.java
@@ -0,0 +1,17 @@
+package dao;
+
+public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+
+    }
+
+    public static DAOFactory getDaoFactory() {
+        if(daoFactory == null) {
+            daoFactory = new DAOFactory();
+        }
+        return daoFactory;
+    }
+
+}
