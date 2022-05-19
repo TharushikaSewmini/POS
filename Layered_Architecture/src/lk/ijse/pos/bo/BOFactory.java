2aedd718ed286b53261cfbd6c48e9d9834c2938b
Tharushika <tharushikasewmini@gmail.com>
2022-05-19 21:06:35 +0530

    Packaging convention added

---
+++ Layered_Architecture/src/lk/ijse/pos/bo/BOFactory.java
@@ -0,0 +1,43 @@
+package lk.ijse.pos.bo;
+
+import lk.ijse.pos.bo.custom.impl.CustomerBOImpl;
+import lk.ijse.pos.bo.custom.impl.ItemBOImpl;
+import lk.ijse.pos.bo.custom.impl.PurchaseOrderBOImpl;
+
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory() {
+
+    }
+
+    // Singleton
+    public static BOFactory getBoFactory() {
+        if(boFactory == null) {
+            boFactory = new BOFactory();
+        }
+        return boFactory;
+    }
+
+    // public final static integer values
+    public enum BOTypes {
+        CUSTOMER, ITEM, PURCHASE_ORDER
+    }
+
+    //method for hide the object creation logic and return what client wants
+    public SuperBO getBO(BOFactory.BOTypes types) {
+        switch(types) {
+            case CUSTOMER:
+                return new CustomerBOImpl(); //SuperBO bo=new CustomerBOImpl();
+
+            case ITEM:
+                return new ItemBOImpl(); //SuperBO bo=new ItemBOImpl();
+
+            case PURCHASE_ORDER:
+                return new PurchaseOrderBOImpl(); //SuperBO bo = new PurchaseOrderBOImpl();
+
+            default:
+                return null;
+        }
+    }
+}
